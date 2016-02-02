85e007d5a308cd69b0f08f76122afa2ea54d9d27
jpbeaudet <jpbeaudet84@gmail.com>
2016-02-01 19:21:40 -0500

    fixed small errors

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,8 @@
 var Config = {
 		host: "localhost",
-		port: 8080
+		port: 8080,
+		google_api_client: "tasksmarket_secret",
+		google_api_key: "AIzaSyA6EdIlY3xCUJXVxWje2aPla2MfqXgX4VA"
 		
 };
 module.exports = Config;
